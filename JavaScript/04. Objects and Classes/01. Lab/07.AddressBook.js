faa9e9b252a0a88d38cb91b25784349cae40dbe9
suleimanapaspalova <157298073+suleimanapaspalova@users.noreply.github.com>
2025-02-21 08:10:05 -0800

    add new

--- JavaScript/04. Objects and Classes/01. Lab/07.AddressBook.js
+++ JavaScript/04. Objects and Classes/01. Lab/07.AddressBook.js
@@ -1,19 +1,12 @@
-function addressBook (list) {
-    let addresses = {};
-
-    for (let i = 0; i < list.length; i++) {
-        let [name, address] = list[i].split(':');
-
-        addresses[name] = address;
-    }   
-
-    for (name of Object.keys(addresses).sort()) {
-        console.log(`${name} -> ${addresses[name]}`);
+function addressBook(input) {
+    let addressBook = {};
+    for (let inputElement of input) {
+        const personInfo = inputElement.toString().split(':');
+        addressBook[personInfo[0]] = personInfo[1];
+    }
+    const sortedBook = Object.entries(addressBook).sort((a, b) => a[0].localeCompare(b[0]));
+    addressBook = Object.fromEntries(sortedBook);
+    for (let personAddress in addressBook) {
+        console.log(`${personAddress} -> ${addressBook[personAddress]}`)
     }
 }
-
-addressBook(['Tim:Doe Crossing',
-    'Bill:Nelson Place',
-    'Peter:Carlyle Ave',
-    'Bill:Ornery Rd']
-   );
